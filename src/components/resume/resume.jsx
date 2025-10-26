ae364475bbcc1719e2899b90942d2682942be152
Nguyen Bui <may.thaonguyen2205@gmail.com>
2025-10-26 15:02:43 -0400

    change resume.jsx

--- src/components/resume/resume.jsx
+++ src/components/resume/resume.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-// import Particle from "../Particle";
-import pdf from "../../assets/May-2025-Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
-// import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import 'react-pdf/dist/Page/AnnotationLayer.css';
-import 'react-pdf/dist/Page/TextLayer.css';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+import pdfFile from "../../assets/resume.pdf";
+import pdfWorker from "pdfjs-dist/build/pdf.worker.min.mjs?url";
+
+pdfjs.GlobalWorkerOptions.workerSrc = pdfWorker;
 
 function Resume() {
   const [width, setWidth] = useState(1200);
@@ -18,28 +18,30 @@ function Resume() {
   }, []);
 
   return (
-    <div>
-      <Container fluid className="resume-section">
-        {/* <Particle /> */}
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
+    <Container fluid className="resume-section">
+      <Row style={{ justifyContent: "center", position: "relative" }}>
+        <Button
+          variant="primary"
+          href={pdfFile}
+          target="_blank"
+          style={{ maxWidth: "250px" }}
+        >
+          <AiOutlineDownload />
+          &nbsp;Download CV
+        </Button>
+      </Row>
 
-        <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
-      </Container>
-    </div>
+      <Row className="resume">
+        <Document
+          file={pdfFile}
+          className="d-flex justify-content-center"
+          loading="Loading PDF... Please wait an itty bitty second. Sit back and have a sip of coffee!"
+          onLoadError={(error) => console.error("PDF load error:", error)}
+        >
+          <Page pageNumber={1} scale={width > 786 ? 1.4 : 0.6} />
+        </Document>
+      </Row>
+    </Container>
   );
 }
 
